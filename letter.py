e62abb0c4b348af19d363a6497efb8d603ebf624
Andrea Bertieri <andreabertieri@MacBook-Air-de-Andrea.local>
2022-09-13 19:45:57 -0500

    Working with Letters

---
+++ letter.py
@@ -0,0 +1,9 @@
+palabra = "La palabra"
+x=len(palabra)
+print("La palabra tiene una longitud de: "+ str(x))
+print("Empieza por: "+palabra[0])
+print("Las últimas tres letras son: "+palabra[7:10])
+print("En mayúsculas se ve así: "+palabra.upper())
+print("Y como no recuerdo que hace esta función: "+palabra.strip())
+print("Finalmente me resulta muy gracioso cambiar las a por o: "+palabra.replace('a','o'))
+
