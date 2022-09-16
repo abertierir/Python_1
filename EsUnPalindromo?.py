8b23fac483cb6e5095f57f5d8f401708818d1eef
Andrea Bertieri <andreabertieri@MacBook-Air-de-Andrea.local>
2022-09-15 20:56:20 -0500

    Palindromo working

--- EsUnPalindromo?.py
+++ EsUnPalindromo?.py
@@ -1,12 +1,13 @@
 def run():
     sentence=input("Ingrese el posible palíndromo: ")
-    sentence=str(sentence).replace(" ","").lower
-    if sentence==sentence[::-1]:
+    sentence=str(sentence).replace(" ","")
+    sentence_invertida=sentence[::-1].lower
+    if sentence==sentence_invertida:
         print("Es un palíndromo")
     else:
         print("No es un palíndromo")
 
    
 
-if(__name__)=="__main__":
+if __name__  == "__main__":
     run()
